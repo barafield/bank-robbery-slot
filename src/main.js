840e040c3ea0a8a5dab34b20e849c8722676a825
Arsênio Paulo <arsenio.paulo0513@gmail.com>
2026-05-12 01:21:16 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './style.css'
 import {
   Application,
   Assets,
   Sprite,
   Graphics,
   AnimatedSprite,
-  Container
+  Container,
+  Text
 } from 'pixi.js'
 
 /* =======================================================
@@ -126,19 +126,179 @@ gameContainer.addChild(fox)
 ======================================================= */
 
 const slotTexture = await Assets.load(
-  getAsset('ui/hud completo.png')
+  getAsset('ui/HUD_.png')
 )
 
 const slotFrame = new Sprite(slotTexture)
 
 slotFrame.anchor.set(0.5)
-slotFrame.width = 1850
-slotFrame.height = 1100
+slotFrame.width = 1590
+slotFrame.height = 1000
 slotFrame.x = BASE_WIDTH / 2
 slotFrame.y = BASE_HEIGHT / 2
 
 gameContainer.addChild(slotFrame)
 
+/* =======================================================
+   INICIO DA FUNÇÃO BET (VALOR DA APOSTA)
+======================================================= */
+let betAmount = 100
+
+const betText = new Text({
+  text: `$ ${betAmount}`,
+  style: {
+  fill: '#7CFF4F',
+  fontSize: 40,
+  fontWeight: 'bold',
+  stroke: '#000000',
+  strokeThickness: 6,
+  dropShadow: true,
+  dropShadowBlur: 4
+  }
+})
+
+betText.anchor.set(0.5)
+
+betText.x = BASE_WIDTH * 0.667
+betText.y = BASE_HEIGHT * 0.92
+
+gameContainer.addChild(betText)
+
+function updateBetText() {
+  betText.text = `$ ${betAmount}`
+}
+
+/* =======================================================
+   AUMENTAR E BAIXAR A POSTA
+======================================================= */
+
+const decreaseBetArea = new Graphics()
+
+decreaseBetArea.circle(0, 0, 55)
+
+decreaseBetArea.fill({
+  color: 0xffffff,
+  alpha: 0.001
+})
+
+decreaseBetArea.x = BASE_WIDTH * 0.76
+decreaseBetArea.y = BASE_HEIGHT * 0.92
+
+decreaseBetArea.eventMode = 'static'
+decreaseBetArea.cursor = 'pointer'
+
+gameContainer.addChild(decreaseBetArea)
+
+const increaseBetArea = new Graphics()
+
+increaseBetArea.circle(0, 0, 55)
+
+increaseBetArea.fill({
+  color: 0xffffff,
+  alpha: 0.001
+})
+
+increaseBetArea.x = BASE_WIDTH * 0.92
+increaseBetArea.y = BASE_HEIGHT * 0.92
+
+increaseBetArea.eventMode = 'static'
+increaseBetArea.cursor = 'pointer'
+
+/* =======================================================
+   VALOR DA APOSTA (ESTABELECIDO 50 A 50)
+======================================================= */
+
+gameContainer.addChild(increaseBetArea)
+
+decreaseBetArea.on('pointerdown', () => {
+   betAmount -= 50
+
+  if (betAmount < 50) {
+    betAmount = 50
+  }
+
+  updateBetText()
+})
+
+increaseBetArea.on('pointerdown', () => {
+  betAmount += 50
+
+  if (betAmount > 5000) {
+    betAmount = 5000
+  }
+
+  updateBetText()
+})
+
+/* =======================================================
+   BOTÃO SPIN APERTADO E NORMAL
+======================================================= */
+
+const spinNormalTexture = await Assets.load(
+  getAsset('ui/spin_button_normal.png')
+)
+
+const spinPressedTexture = await Assets.load(
+  getAsset('ui/spin_button_pressed.png')
+)
+
+const spinButton = new Sprite(spinNormalTexture)
+
+spinButton.anchor.set(0.5)
+spinButton.x = BASE_WIDTH * 0.84
+spinButton.y = BASE_HEIGHT * 0.92
+spinButton.width = 380
+spinButton.height = 280
+
+spinButton.eventMode = 'none'
+
+gameContainer.addChild(spinButton)
+
+/* =======================================================
+   ÁREA INVISÍVEL DO BOTÃO SPIN
+======================================================= */
+const spinHitArea = new Graphics()
+
+spinHitArea.circle(0, 0, 85)
+
+spinHitArea.fill({
+  color: 0xffffff,
+  alpha: 0.001
+})
+
+spinHitArea.x = spinButton.x
+spinHitArea.y = spinButton.y + 10
+
+spinHitArea.eventMode = 'static'
+spinHitArea.cursor = 'pointer'
+
+gameContainer.addChild(spinHitArea)
+
+gameContainer.setChildIndex(fox, gameContainer.children.length - 2)
+gameContainer.setChildIndex(spinButton, gameContainer.children.length - 1)
+
+const SPIN_BUTTON_X = BASE_WIDTH * 0.84
+const SPIN_BUTTON_Y = BASE_HEIGHT * 0.92
+
+function pressSpinButton() {
+  spinButton.texture = spinPressedTexture
+  spinButton.x = SPIN_BUTTON_X
+  spinButton.y = SPIN_BUTTON_Y + 10
+}
+
+function releaseSpinButton() {
+  spinButton.texture = spinNormalTexture
+  spinButton.x = SPIN_BUTTON_X
+  spinButton.y = SPIN_BUTTON_Y
+}
+
+spinHitArea.on('pointerdown', () => {
+  if (isSpinning) return
+
+  pressSpinButton()
+  spinReels()
+   
+})
 /* =======================================================
    ANIMAÇÕES DOS SÍMBOLOS
 ======================================================= */
@@ -163,17 +323,18 @@ const symbolKeys = Object.keys(symbolAnimations)
 ======================================================= */
 
 const symbolScaleMap = {
-  bank: 1.0,
+  bank: 0.90,
   dynamite: 1.45,
   safe: 1.15,
-  handcuffs: 1.1,
-  cell: 1.0,
+  handcuffs: 0.95,
+  cell: 0.95,
   A: 0.95,
   K: 0.95,
   Q: 0.95,
   J: 0.95,
   ten: 0.95
 }
+
 
 /* =======================================================
    MATRIZ DOS REELS
@@ -199,17 +360,15 @@ const REEL_OFFSET_Y = -5
 const GAP_X = REEL_AREA_WIDTH / COLS
 const GAP_Y = REEL_AREA_HEIGHT / ROWS
 
-const SYMBOL_SIZE = Math.min(GAP_X, GAP_Y) * 1.1
+const SYMBOL_SIZE = Math.min(GAP_X, GAP_Y) * 1.0
 
 const startX =
   BASE_WIDTH / 2 - REEL_AREA_WIDTH / 2 + GAP_X / 2 + REEL_OFFSET_X
 
-const REEL_CENTER_Y = BASE_HEIGHT / 2 + 15
+const REEL_CENTER_Y = BASE_HEIGHT / 2.05 + 15
 
 const startY =
   REEL_CENTER_Y - REEL_AREA_HEIGHT / 2 + GAP_Y / 2 + REEL_OFFSET_Y
-
-const reelSymbols = []
 
 function applySymbolSize(symbol, symbolKey) {
   const individualScale = symbolScaleMap[symbolKey] || 1
@@ -234,11 +393,57 @@ function getRandomSymbolKey() {
 }
 
 /* =======================================================
-   CRIAÇÃO DOS REELS
+   MÁSCARA DA ÁREA DOS REELS
+   -------------------------------------------------------
+   Esconde símbolos que passam para fora da roleta.
 ======================================================= */
 
-reelMatrix.forEach((row, rowIndex) => {
-  row.forEach((symbolKey, colIndex) => {
+const reelsMask = new Graphics()
+
+reelsMask.rect(
+  startX - GAP_X / 2,
+  startY - GAP_Y / 2,
+  REEL_AREA_WIDTH,
+  REEL_AREA_HEIGHT
+)
+
+reelsMask.fill({
+  color: 0xffffff,
+  alpha: 1
+})
+
+gameContainer.addChild(reelsMask)
+
+/* =======================================================
+   CRIAÇÃO DOS REELS COM CONTAINERS
+   -------------------------------------------------------
+   Agora cada coluna é um Container independente.
+   Isso permite criar efeito de rolo/esteira de cassino.
+======================================================= */
+
+const reelContainers = []
+const reelSymbols = []
+
+const EXTRA_SYMBOLS = 2
+const SYMBOLS_PER_COLUMN = ROWS + EXTRA_SYMBOLS
+
+for (let col = 0; col < COLS; col++) {
+  const reelContainer = new Container()
+
+  reelContainer.x = startX + col * GAP_X
+  reelContainer.y = startY
+
+  reelContainer.mask = reelsMask
+
+  reelContainers.push(reelContainer)
+  gameContainer.addChild(reelContainer)
+
+  const columnSymbols = []
+
+  for (let row = 0; row < SYMBOLS_PER_COLUMN; row++) {
+    const symbolKey =
+      reelMatrix[row % ROWS][col]
+
     const symbol = new AnimatedSprite(symbolAnimations[symbolKey])
 
     symbol.anchor.set(0.5)
@@ -247,101 +452,120 @@ reelMatrix.forEach((row, rowIndex) => {
 
     applySymbolSize(symbol, symbolKey)
 
-    symbol.x = startX + colIndex * GAP_X
-    symbol.y = startY + rowIndex * GAP_Y
+    symbol.x = 0
+    symbol.y = row * GAP_Y
 
-    reelSymbols.push(symbol)
-    gameContainer.addChild(symbol)
-  })
-})
+    symbol.currentKey = symbolKey
 
-/* =======================================================
-   RAPOSA POR CIMA
-======================================================= */
+    columnSymbols.push(symbol)
+    reelContainer.addChild(symbol)
+  }
 
-gameContainer.setChildIndex(fox, gameContainer.children.length - 1)
+  reelSymbols.push(columnSymbols)
+}
 
 /* =======================================================
-   ÁREA INVISÍVEL DO BOTÃO SPIN
-======================================================= */
-
-const spinArea = new Graphics()
-
-spinArea.rect(-200, 50, 150, 150)
-spinArea.fill({
-  color: 0xffffff,
-  alpha: 0.001
-})
-
-spinArea.x = BASE_WIDTH * 0.81
-spinArea.y = BASE_HEIGHT * 0.805
-
-spinArea.eventMode = 'static'
-spinArea.cursor = 'pointer'
-
-gameContainer.addChild(spinArea)
-
-/* =======================================================
-   SPIN
+   CONTROLE DE SPIN
 ======================================================= */
 
 let isSpinning = false
 
-function spinReels() {
-  if (isSpinning) return
+/* =======================================================
+   SPIN COM EFEITO DE ROLO
+======================================================= */
 
-  isSpinning = true
+function spinColumn(reelContainer, columnSymbols, columnIndex, onFinish) {
+  let elapsed = 0
 
-  let spinCount = 0
+  const duration = 1200 + columnIndex * 300
+  const speed = 42
 
-  const maxSpinCount = 30
-  const spinSpeed = 55
+  const originalY = reelContainer.y
 
-  const spinInterval = setInterval(() => {
-    reelSymbols.forEach((symbol) => {
-      const randomKey = getRandomSymbolKey()
+  const spinTicker = () => {
+    elapsed += app.ticker.deltaMS
 
-      symbol.textures = symbolAnimations[randomKey]
-      symbol.gotoAndPlay(0)
+    columnSymbols.forEach((symbol) => {
+      symbol.y += speed
 
-      applySymbolSize(symbol, randomKey)
+      if (symbol.y >= GAP_Y * SYMBOLS_PER_COLUMN) {
+        symbol.y -= GAP_Y * SYMBOLS_PER_COLUMN
+
+        const randomKey = getRandomSymbolKey()
+
+        symbol.textures = symbolAnimations[randomKey]
+        symbol.gotoAndPlay(0)
+
+        applySymbolSize(symbol, randomKey)
+        symbol.currentKey = randomKey
+      }
     })
 
-    spinCount++
+    if (elapsed >= duration) {
+      app.ticker.remove(spinTicker)
 
-    if (spinCount >= maxSpinCount) {
-      clearInterval(spinInterval)
-
-      reelSymbols.forEach((symbol) => {
+      columnSymbols.forEach((symbol, index) => {
         const finalKey = getRandomSymbolKey()
 
         symbol.textures = symbolAnimations[finalKey]
         symbol.gotoAndPlay(0)
 
         applySymbolSize(symbol, finalKey)
+        symbol.currentKey = finalKey
+
+        symbol.y = index * GAP_Y
       })
 
-      isSpinning = false
-      console.log('SPIN finalizado')
+      reelContainer.y = originalY + 10
+
+      setTimeout(() => {
+        reelContainer.y = originalY
+        onFinish()
+      }, 90)
     }
-  }, spinSpeed)
+  }
+
+  app.ticker.add(spinTicker)
 }
 
+function spinReels() {
+  if (isSpinning) return
+
+  isSpinning = true
+
+  let finishedColumns = 0
+
+  reelContainers.forEach((reelContainer, columnIndex) => {
+    spinColumn(
+      reelContainer,
+      reelSymbols[columnIndex],
+      columnIndex,
+      () => {
+        finishedColumns++
+
+        if (finishedColumns === reelContainers.length) {
+          isSpinning = false
+          releaseSpinButton()
+          console.log('SPIN finalizado com efeito de rolo')
+        }
+      }
+    )
+  })
+}
+
+
+
 /* =======================================================
-   EVENTOS DO BOTÃO SPIN
+   EVENTOS DO BOTÃO SPIN - Atualizar
 ======================================================= */
 
-spinArea.on('pointerdown', () => {
-  spinArea.scale.set(0.97)
-})
+spinHitArea.on('pointerdown', () => {
+  console.log('CLICOU NO SPIN')
 
-spinArea.on('pointerup', () => {
-  spinArea.scale.set(1)
+  if (isSpinning) return
+
+  pressSpinButton()
   spinReels()
-})
-
-spinArea.on('pointerupoutside', () => {
-  spinArea.scale.set(1)
 })
 
 /* =======================================================
@@ -350,3 +574,4 @@ spinArea.on('pointerupoutside', () => {
 
 resizeGame()
 window.addEventListener('resize', resizeGame)
+
